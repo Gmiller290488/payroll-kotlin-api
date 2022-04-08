e56f12b3470f7175578b50bf7f794f4cb9119f21
Gareth Miller <gmiller290488@gmail.com>
2022-04-08 09:07:14 +0100

    initial commit. spring boot initialiser

---
+++ src/test/kotlin/com/gm/payroll/PayrollApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gm.payroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PayrollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
